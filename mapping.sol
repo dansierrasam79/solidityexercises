66600cf1132c5d7dfdc6818c152142209ef63c45
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2025-10-25 19:34:06 +0530

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -1,1 +1,27 @@
 
+//Part 4: Mapping
+pragma solidity ^0.6.0
+contract MyContract {
+	//Mappings == Python dictionary == Java Hashmap
+	mapping(uint => string) public names;
+	mapping(uint => Book) public books;
+	mapping(address => mapping(uint => Book)) public myBooks;
+	struct Book {
+	string title;
+	string Author;
+	}
+
+	constructor() public {
+	names[1] = "Adam";
+	names[2] = "Bruce";
+	names[3] = "Carl";
+	}
+
+	function addBook(uint _id, string memory _title, string memory _author) public {
+		books[_id] = Book(_title, _author);
+	}
+
+	function addMyBook(uint _id, string memory _title, string memory _author) public {
+		myBooks[msg.sender][_id] = Book(_title, _author); 
+	}
+}
