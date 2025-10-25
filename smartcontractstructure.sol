41635ab4a5a058b3b1de122b28d92d5a6008ab78
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2025-10-25 19:30:24 +0530

    Update smartcontractstructure.sol

--- smartcontractstructure.sol
+++ smartcontractstructure.sol
@@ -1,1 +1,17 @@
+// Part 1: The structure of a smart contract
+pragma solidity ^0.6.0;
+contract Counter {
+
+constructor() public {
+count = 0;
+}
+// the "view" modifier is used whenever the value returned from this function can only be seen but not altered
+function getCount() public view returns(uint) {
+return count;
+}
+
+function incrementCount() public {
+count ++;
+}
+}
 
