a1256331c4e9a69869e30370a147237938ef43f7
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2025-11-02 12:17:08 +0530

    Create Exercise4LearnVariables.sol

---
+++ Exercise4LearnVariables.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Exercise 
+// 1. create a new variable called wallet as an integer 
+// 2. create a boolean called spend
+// 3. create a string give it the name notifySpend
+// 4. initialize the wallet to 500 
+// 5. set the value of spend to false 
+// 6. add the string literal (the string value ) 'you have spent money' to notifySpend 
+pragma solidity <= 0.8.19;
+contract learnVariables {
+
+    uint chocolateBar = 10;
+    uint storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = "Error! There has been a mistake!";
+
+    uint wallet = 500;
+    bool spend = false;
+    string notifySpend = 'you have spent money';
+}
