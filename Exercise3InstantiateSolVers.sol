53bf212eee73a67182f899c1dfc97ba03fa0a41d
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2025-11-02 12:16:21 +0530

    Create Exercise3InstantiateSolVers.sol

---
+++ Exercise3InstantiateSolVers.sol
@@ -0,0 +1,6 @@
+// Exercise: create a new Solidity file called exercise assignment
+// Instantiate the solidity version of 0.5.0
+// Bonus: increase the range of the version from 0.5.0 upto 0.6.2
+// Solution:
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 <= 0.8.18;
